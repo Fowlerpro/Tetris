f3f74f4ebc2cc773c0b4c0785c5f5843717f0482
Fowlerpro <jaycefowler@outlook.com>
2026-02-08 18:31:41 -0500

    Add project files.

---
+++ Assets/scripts/UIController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+
+    public TetrisManager tetrisManager;
+
+    public GameObject endGamePanel;
+
+    public void UpdateScore()
+    {
+        scoreText.text = $"SCORE: {tetrisManager.score:n0}";
+    }
+
+    public void UpdateGameOver()
+    {    
+        endGamePanel.SetActive(tetrisManager.gameOver);
+    }
+
+    public void PlayAgain()
+    {   
+        tetrisManager.SetGameOver(false);
+    }
+}
