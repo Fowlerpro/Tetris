7c72253429b70874615f0792faa679bb7dfe0802
Fowlerpro <jaycefowler@outlook.com>
2026-02-08 18:45:20 -0500

    Add Y Tertronimo

--- Assets/scripts/TetronimoData.cs
+++ Assets/scripts/TetronimoData.cs
@@ -4,8 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-public enum Tetronimo { I, O, T, J, L, S, Z }
+public enum Tetronimo { I, O, T, J, L, S, Z, Y }
 [Serializable]
+//added Y piece to the enum
 public struct TetronimoData
 {
     public Tetronimo tetronimo;
